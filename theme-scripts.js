b03cbf68a53742ec26b2ea5b8c68db8874f97dcf
shg55 <shg55@pitt.edu>
2019-03-04 20:28:08 -0500

    Add files via upload

---
+++ theme-scripts.js
@@ -0,0 +1,46 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 150) {
+        $('.navbar').addClass('navbar-shrink');
+    }
+    else {
+        $('.navbar').removeClass('navbar-shrink');
+    }
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+  $("#back-top").hide();
+
+  $(window).scroll(function () {
+  	if ($(this).scrollTop() > 100) {
+  		$('#back-top').fadeIn();
+  	} else {
+  		$('#back-top').fadeOut();
+  	}
+  });
+
+  $('#back-top a').on("click", function(){
+  	$('body,html').animate({
+  		scrollTop: 0
+  	}, 800);
+  	return false;
+  });
+
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
